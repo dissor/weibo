862e9f308aa5cddd4f7a8cb06f0ba1edd557f2b1
dissor <guojianwenjonas@foxmail.com>
2022-11-09 23:02:00 +0800

    GET 取得数据成功

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+# url
+url = "https://m.weibo.cn/comments/hotflow?"
+
+# 请求头
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+}
+
+# 参数
+params = {
+    "id" : 4823340205736691,
+    "mid" : 4823340205736691,
+    # "max_id" : 139275305686179,
+    "max_id_type" : 0
+}
+
+r = requests.get(url, headers = headers, params = params)
+
+print(r.content.decode('utf8'))
